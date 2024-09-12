4282fab14b2fbc3aeb9fea0d1384016c4cff9a0c
andre <108833211+andredomareski@users.noreply.github.com>
2024-09-12 08:52:50 -0300

    1209

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Sabrina", "Monica"];
+const nomes = ["Joao", "Andre", "Juliano"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()*lista.length);   
